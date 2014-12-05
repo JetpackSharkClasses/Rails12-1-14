c4da560058fac662873d5b54adec8671fc43d72a
Remy Porter <remyporter@jetpackshark.com>
2014-12-05 16:02:12 -0500

    Finally can post stuff!

---
+++ app/controllers/microposts_controller.rb
@@ -0,0 +1,17 @@
+class MicropostsController < ApplicationController
+
+	def create
+		@micropost = 
+			current_user.microposts.build(micropost_params)
+		if @micropost.save
+			redirect_to current_user
+		else
+			#???
+		end
+	end
+
+	private
+		def micropost_params
+			params.require(:micropost).permit(:content)
+		end
+end
